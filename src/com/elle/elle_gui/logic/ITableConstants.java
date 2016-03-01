b6782cec32aaf98f1b8351cb7506a6535572c4af
cigreja <cigreja@live.com>
2016-03-01 15:30:25 -0500

    fixed an error. Added a column width for the new column that was added.

--- src/com/elle/elle_gui/logic/ITableConstants.java
+++ src/com/elle/elle_gui/logic/ITableConstants.java
@@ -45,6 +45,7 @@ public interface ITableConstants {
         40, // How
         20, // wash
         55, // Codes
+        55, // ksflag
         65, // Account
         65, // L_codes
         58, // SecType
