59edd0073241db655eecb6d43e63d1b33982678c
cigreja <cigreja@live.com>
2016-04-21 17:21:14 -0400

    new version 1.0.1 : #618 default username and password

--- src/com/elle/elle_gui/logic/ITableConstants.java
+++ src/com/elle/elle_gui/logic/ITableConstants.java
@@ -20,7 +20,7 @@ public interface ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
     public static final String CREATION_DATE = "2016-04-21";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     // account names
     public static final String IB9048_ACCOUNT_NAME = "IB9048";
